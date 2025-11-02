8eee6620bbf7ee0fc0c4218579bd4e9c74b6e005
Shany <shanycsabu@gmail.com>
2025-11-02 22:42:47 +1300

    add-allure-report

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,12 +22,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+        ['list'], // Keep the default list reporter
+        //['html'], // Keep the default HTML reporter
+        ["line"],
+        ["allure-playwright"]
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
